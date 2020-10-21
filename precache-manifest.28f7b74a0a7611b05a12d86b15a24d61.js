fa6224e5c5b64da25b36ed39ecb8e00fa5475884
Jericho Irvin <aldejerichoirvin@gmail.com>
2020-10-21 17:27:26 +0800

    Updates

---
+++ precache-manifest.28f7b74a0a7611b05a12d86b15a24d61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd29b6d6fbc2b685ec3dcade4be0d428",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8707d1edc7ca15d5409f",
+    "url": "/monsters-rolodex/static/css/main.ef59cedb.chunk.css"
+  },
+  {
+    "revision": "41f8b8672aad5322edd3",
+    "url": "/monsters-rolodex/static/js/2.e88ff5fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.e88ff5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8707d1edc7ca15d5409f",
+    "url": "/monsters-rolodex/static/js/main.8c1de679.chunk.js"
+  },
+  {
+    "revision": "676fa6fe07c881f9e750",
+    "url": "/monsters-rolodex/static/js/runtime-main.9c224c16.js"
+  }
+]);
